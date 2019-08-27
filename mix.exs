7f94d9fc0e09a76fae5c5ac0d4044291f80a1dcd
Ivan <ivan@no-wire.com>
2019-08-27 16:03:32 +0200

    Remove ambiguity

--- mix.exs
+++ mix.exs
@@ -8,9 +8,9 @@ defmodule Vimeo.Mixfile do
       elixir: "~> 1.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
-      description: description,
-      package: package,
+      deps: deps(),
+      description: description(),
+      package: package(),
       docs: [extras: ["README.md"]],
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test]
